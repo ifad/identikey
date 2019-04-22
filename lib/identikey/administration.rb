6c6c4856111cc8b3e11b7d45b8ed4e7a1b7844fc
Marcello Barnaba <vjt@openssl.it>
2019-04-22 16:46:27 +0200

    Use Savon operations, add generic response parser

--- lib/identikey/administration.rb
+++ lib/identikey/administration.rb
@@ -13,8 +13,10 @@ module Identikey
       log_level: :debug,
       pretty_print_xml: true
 
-    def logon(username, password, domain: 'master')
-      client.call(:logon, message: {
+    operations :logon, :logoff
+
+    def logon(username:, password:, domain: 'master')
+      resp = super(message: {
         attributeSet: {
           attributes: [
             { attributeID: 'CREDFLD_DOMAIN',
@@ -36,10 +38,12 @@ module Identikey
           ]
         }
       })
+
+      parse_response resp, :logon_response
     end
 
-    def logoff(sid)
-      client.call(:logoff, message: {
+    def logoff(session_id:)
+      resp = super(message: {
         attributeSet: {
           attributes: [
             { attributeID: 'CREDFLD_SESSION_ID',
@@ -48,7 +52,46 @@ module Identikey
           ]
         }
       })
+
+      parse_response resp, :logoff_response
     end
 
+    def parse_response(resp, root_element)
+      body = resp.body
+
+      if body.size.zero?
+        raise Identikey::Error, "Empty response received"
+      end
+
+      unless body.key?(root_element)
+        raise Identikey::Error, "Expected response to have #{root_element}, found #{body.keys.join(', ')}"
+      end
+
+      unless body[root_element].key?(:results)
+        raise Identikey::Error, "Results element not found below #{root_element}"
+      end
+
+      results = body[root_element][:results]
+
+      unless results.key?(:result_codes)
+        raise Identikey::Error, "Result codes not found below #{root_element}"
+      end
+
+      result_code = results[:result_codes][:status_code_enum] || 'STAT_UNKNOWN'
+
+      unless results.key?(:result_attribute)
+        raise Identikey::Error, "Result attribute not found below #{root_element}"
+      end
+
+      result_attributes = if (attrs = results[:result_attribute].key?(:attributes))
+        results[:result_attribute][:attributes].inject({}) do |h, attribute|
+          h.update(attribute.fetch(:attribute_id) => attribute.fetch(:value))
+        end
+      else
+        {}
+      end
+
+      return result_code, result_attributes
+    end
   end
 end
